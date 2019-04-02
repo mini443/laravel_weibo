43a006ab25f27a5a38110857635245eba0ab4a79
minininja <834487783@qq.com>
2019-04-02 10:27:40 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class UsersController extends Controller
 {
     //创建用户
     public function create(){
         return view('users.create');
     }
+
+    //所有用户
+    public function index(){
+        return '主页';
+    }
+
+    /**
+     * 单个用户
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(User $user)
+    {
+        $user=compact('user');
+//        dump($user->name);
+//        exit();
+        return view('users.show', $user);
+    }
+
 }
