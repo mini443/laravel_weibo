ee3c75e5d89a25b946ec0d6d4c58133e7fcd75c4
minininja <834487783@qq.com>
2019-04-02 11:09:26 +0000

    设置中文错误提示信息

---
+++ resources/lang/cn/validation.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gx
+ * Date: 2019/4/2
+ * Time: 19:01
+ */
+return [
+    'custom' => [
+        'email' => [
+            'required' => '邮箱地址不能为空！',
+        ],
+    ],
+];
