903b7c01b159ae5344870ee3426015d262830908
minininja <834487783@qq.com>
2019-04-04 11:04:43 +0000

    用户列表并分页

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,7 +19,7 @@ class UsersController extends Controller
 
     //所有用户
     public function index(){
-        $users=User::all();
+        $users=User::paginate(10);
         return view('users.index',compact('users'));
     }
 
